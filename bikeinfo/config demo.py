e3ef1c7e6e0408e56caf538e3d1fc7745fe2a661
wufangxun106334-pix <wufangxun106334@gmail.com>
2026-02-12 22:31:19 +0000

    config demo

---
+++ bikeinfo/config demo.py
@@ -0,0 +1,14 @@
+# Database Configuration
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "yours"
+DB_NAME = "yours"
+
+# Dublin Bike API Configuration
+BIKE_API_KEY = "yours"
+BIKE_STATUS_URL = (
+    f"https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey={BIKE_API_KEY}"
+)
+
+# Path Configuration
+FOLDER_PATH = "/yours"
